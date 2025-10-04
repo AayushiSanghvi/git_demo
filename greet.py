17ce6a9db9a82fc6c839efe01656c9159b1d948d
Aayushi Sanghvi <140320416+AayushiSanghvi@users.noreply.github.com>
2025-10-04 21:01:32 +0530

    made a full greeting application for user

--- greet.py
+++ greet.py
@@ -1,2 +1,4 @@
-name=input("enter your name")
-print("Hello world", name)
+name=input("enter your name"))
+print("Hello", name)
+r=input("How are you doing?")
+print("Nice to hear. I am doing good. Thank you for asking."))
